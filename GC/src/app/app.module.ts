f4bccc2aaea1da2563f28fb17b3e22da38013537
PieroMarquez <89226508+PieroMarquez@users.noreply.github.com>
2023-05-04 20:04:19 -0500

    innit commit
    Creación de proyecto

---
+++ GC/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
+import { MatCardModule}from'@angular/material/card';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { LandingPageComponent } from './component/landing-page/landing-page.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
